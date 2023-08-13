560343ae49c99c6b7201d3fb97db43165ab20d0e
junsu <cpzzansu@gmail>
2023-08-13 17:56:12 +0900

    main

---
+++ src/main/java/com/ksmart47/cbtool/CbtoolApplication.java
@@ -0,0 +1,13 @@
+package com.ksmart47.cbtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbtoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksmart47/cbtool/CbtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksmart47.cbtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
